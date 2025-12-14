8df46b5d68180d86e9bbbbb26283d22619c4eeab
lrpv2005-code <lrpv2005@gmail.com>
2025-12-13 21:52:56 -0400

    Add files via upload

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,3 @@
-from django.http import HttpResponse
-# Create your views here.
-def hello(request):
-    return HttpResponse("Hola mundo")
+from django.shortcuts import render
 
-def about(request):
-    return HttpResponse("Request")
+# Create your views here.
